2755c3c3a6ad1e46a1781d8ce8bd66c08caefd60
Patryk <patryk@freewayit.pl>
2025-11-04 10:27:29 +0100

    .

---
+++ FreewayIT.Test/Controllers/CityController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using FreewayIT.Test.DAL;
+
+namespace FreewayIT.Test.Controllers
+{
+    public class CityController : Controller
+    {
+        private TestFreewayITEntities db = new TestFreewayITEntities();
+
+        //
+        // GET: /City/
+
+        public ActionResult Index()
+        {
+            return View(db.Cities.ToList());
+        }
+    }
+}
---
+++ FreewayIT.Test/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="FreewayIT.Test.MvcApplication" Language="C#" %>
